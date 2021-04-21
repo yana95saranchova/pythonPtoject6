2d1f60d9807840ef0da5634f7f9f49e536747302
yana95saranchova <yana.saranchova95@gmail.com>
2021-04-21 14:53:33 +0300

    init1

---
+++ lessson3/task4.py
@@ -0,0 +1,8 @@
+print("solve the equation a*2=6")
+a = float(input("a= "))
+answer=3
+
+if (a!=answer):
+    print(f"it is incorrect, answer is {answer}")
+if (a == answer):
+    print(f"You are right, answer is {answer}")
